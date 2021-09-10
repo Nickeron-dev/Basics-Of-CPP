ded4383f26ff7630bbd637b5e1af09252f8acd71
Nickeron-dev <koshkin061969@gmail.com>
2021-09-10 22:28:00 +0300

    Wrote a small program that takes a number and checks if it is even or odd. Actially when you input wrong values it does not throw an exception, it just puts 0.

---
+++ firstCppCourse/IfElseStatements.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main() {
+  std::cout << "Input whole number: " << '\n';
+  int inputtedNumber;
+  std::cin >> inputtedNumber;
+  if (inputtedNumber % 2 == 0) {
+    std::cout << "Your number: " << inputtedNumber << " is even" << std::endl;
+  } else {
+    std::cout << "Your number: " << inputtedNumber << " is odd" << "\n";
+  }
+  return 0;
+}
