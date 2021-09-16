c90a8d8ce0bcec9e5f6a08e58d3b6a56af5ea3d7
Nickeron-dev <koshkin061969@gmail.com>
2021-09-16 19:43:09 +0300

    Created a little example of using header fils in c++.

---
+++ firstCppCourse/headers-try/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../headers/header.h"
+
+int main() {
+        print_message("Hello");
+        return 0;
+}
---
+++ firstCppCourse/headers-try/src/method_container.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print_message(std::string message) {
+        std::cout << message << std::endl;
+}
