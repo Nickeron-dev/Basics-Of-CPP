5d167a3c3f7cba8e69185e7c05ac7ed86a48714f
Nickeron-dev <koshkin061969@gmail.com>
2021-09-23 20:47:24 +0300

    Wrote many exaples of using vector.

--- cpp-bjarne-stroustrup-book/chapter03/vectors.cpp
+++ cpp-bjarne-stroustrup-book/chapter03/vectors.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 
+//using vect = std::vector;
+namespace v = std;
+
 struct Vertex {
 	float x, y, z; // in structs default modifier is public
 	
@@ -36,7 +39,114 @@ int main() {
 	ver.emplace_back(1, 2, 3);
 	ver.emplace_back(4, 5, 6);
 	ver.emplace_back(7, 8, 9);
+	
+	std::cout << "=============" << std::endl;
 
-	std::cin.get(); // getting input from console
+	std::vector<int> ints; // primitives can be added
+	ints.push_back(5);
+	ints.push_back(10);
+	ints.emplace_back(20);
+	ints.emplace_back(22);
+	
+	std::cout << "Just begin()\n";
+	for (auto i = ints.begin(); i != ints.end(); i++) {
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+	
+	std::cout << "cbegin()" << std::endl;
+	for (auto i = ints.cbegin(); i != ints.cend(); i++) {
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "rbegin()" << std::endl;
+	for (auto i = ints.rbegin(); i != ints.rend(); i++) {
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "crbegin()" << std::endl;
+	for (auto i = ints.crbegin(); i != ints.crend(); i++) {
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+
+	
+	std::cout << "=============" << std::endl;
+
+	std::vector<float> floats;
+	floats.reserve(10);
+//	floats.resize(5);
+	std::cout << "Capacity: " << floats.capacity() << std::endl;
+	std::cout << "max_size: " << floats.max_size() << std::endl;
+	std::cout << floats.empty() << std::endl;
+	std::cout << floats.size() << std::endl;
+
+	for (int i = 0; i < 8; i++) {
+		floats.emplace_back(i);
+	}
+	std::cout << std::endl;
+
+	std::cout << "Capacity: " << floats.capacity() << std::endl;
+        std::cout << "max_size: " << floats.max_size() << std::endl;
+        std::cout << floats.empty() << std::endl;
+        std::cout << floats.size() << std::endl;
+//	floats.resize(5);
+	for (auto i = floats.begin(); i != floats.end(); i++) {
+		std::cout << *i << " ";
+	}
+
+	std::cout << std::endl;
+	floats.resize(5);
+        for (auto i = floats.cbegin(); i != floats.cend(); i++) {
+                std::cout << *i << " ";
+        }
+	std::cout << std::endl;
+	
+	for (int i = 0; i < 2; i++) {
+		floats.pop_back();
+	}
+
+	for (auto i = floats.cbegin(); i != floats.cend(); i++) {
+                std::cout << *i << " ";
+        }
+        std::cout << std::endl;
+	
+	floats.clear();
+	for (auto i = floats.cbegin(); i != floats.cend(); i++) {
+                std::cout << *i << " ";
+        }
+	floats.push_back(5);
+        std::cout << std::endl;
+	std::cout << "Hello";	
+	int value = 10;
+	floats.resize(5);
+	std::cout << "Capacity: " << floats.capacity() << std::endl;
+        std::cout << "max_size: " << floats.max_size() << std::endl;
+        std::cout << floats.empty() << std::endl;
+        std::cout << floats.size() << std::endl;
+
+	for (auto i = floats.begin(); i != floats.end(); i++) {
+		floats.insert(i, value++);
+	}
+	std::cout << "Hello";
+	for (auto i = floats.cbegin(); i != floats.cend(); i++) {
+                std::cout << *i << " ";
+        }
+	std::cout << "Hello";
+        std::cout << std::endl;	
+	try {
+		std::cout << floats.at(0) << " ";
+		std::cout << floats.at(1) << " ";
+		std::cout << floats.at(2) << " ";
+		std::cout << floats.at(3) << " ";
+	} catch(std::out_of_range) {
+		std::cout << "error\n";
+	}
+	std::cout << "\nBack(): " << floats.back() << std::endl;
+	std::cout << "Front(): " << floats.front() << std::endl;
+	std::cout << "Data(): " << floats.data() << std::endl; 
+//	std::cin.get(); // getting input from console
 	return 0;
 }
