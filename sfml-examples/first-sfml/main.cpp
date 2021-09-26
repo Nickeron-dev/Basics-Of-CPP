99bc966b682e66ac8b0fd4734b5cd09d512a7228
Nickeron-dev <koshkin061969@gmail.com>
2021-09-26 22:14:59 +0300

    Implemented MVP of my clicker program for Unix. Since it doesn't click on dynamic positions without changing the cursor position, I decided to make it click only on one position without changing cursor.

--- sfml-examples/first-sfml/main.cpp
+++ sfml-examples/first-sfml/main.cpp
@@ -1,10 +1,6 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
 #include <ApplicationServices/ApplicationServices.h>
-#include <unistd.h>
-//#include <QTest>
-
-extern "C" bool click();
+#include <thread>
 
 int main()
 {
@@ -28,80 +24,44 @@ int main()
 //        window.draw(shape);
 //        window.display();
 //    }
-    __CGEvent *event;
-// while there are pending events...
+
     bool state = false;
-        while (true)
-        {
-            if (state) {
-                //First Variant
-//                CGPoint pt;
-////                pt.x = sf::Mouse::getPosition().x;
-////                pt.y = sf::Mouse::getPosition().y;
-//                pt.x = 300;
-//                pt.y = 300;
-//
-//                CGPostMouseEvent( pt, 1, 1, 1 );
-//                CGPostMouseEvent( pt, 1, 1, 0 );
-//                sleep(1);
+    while (true)
+    {
+        if (state) {
+            CGPoint pt;
+            pt.x = 300;
+            pt.y = 300;
 
+            CGPostMouseEvent( pt, 0, 1, 1 );
+            //sleep(1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            CGPostMouseEvent( pt, 0, 1, 0 );
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
-                //int x = sf::Mouse::getPosition().x;
-                //int y = sf::Mouse::getPosition().y;
-                //CGEventRef not_real = CGEventCreate(NULL);
-                //CGPoint cursor = CGEventGetLocation(not_real);
-                //CFRelease(event);
-                //int x = cursor.x;
-                //int y = cursor.y;
-                CGEventRef click1_down = CGEventCreateMouseEvent(
-                        NULL, kCGEventLeftMouseDown,
-                        CGPointMake(250, 250),
-                        kCGMouseButtonLeft
-                );
-                // Left button up at 250x250
-                CGEventRef click1_up = CGEventCreateMouseEvent(
-                        NULL, kCGEventLeftMouseUp,
-                        CGPointMake(250, 250),
-                        kCGMouseButtonLeft
-                );
-                CGEventPost(kCGHIDEventTap, click1_down);
-                CGEventPost(kCGHIDEventTap, click1_up);
-                sleep(1);
-                // Release the events
-                CFRelease(click1_up);
-                CFRelease(click1_down);
-                std::cout << "X = " << 250 << " Y = " << 250 << std::endl;
-                //state = false;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::K)) {
-                state = true;
-            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::O)) {
-                state = false;
-            }
+            // Following variant is also possible but it enforces me to change the position of my mouse
+//                CGEventRef click1_down = CGEventCreateMouseEvent(
+//                        NULL, kCGEventLeftMouseDown,
+//                        CGPointMake(x, y),
+//                        kCGMouseButtonLeft
+//                );
+            // Left button up at 250x250
+//                CGEventRef click1_up = CGEventCreateMouseEvent(
+//                        NULL, kCGEventLeftMouseUp,
+//                        CGPointMake(x, y),
+//                        kCGMouseButtonLeft
+//                );
+            //CGEventPost(kCGHIDEventTap, click1_down);
+            //CGEventPost(kCGHIDEventTap, click1_up);
+            //sleep(1);
+            // Release the events
+            //CFRelease(click1_up);
+            //CFRelease(click1_down);
         }
-}
-
-extern "C" bool click() {
-    //__CGEvent *event;
-    //CGEventSetIntegerValueField(event, kCGMouseEventClickState, 1);
-    /*
-    CGEventRef click1_down = CGEventCreateMouseEvent(
-            NULL, kCGEventLeftMouseDown,
-            CGPointMake(250, 250),
-            kCGMouseButtonLeft
-    );
-    // Left button up at 250x250
-    CGEventRef click1_up = CGEventCreateMouseEvent(
-            NULL, kCGEventLeftMouseUp,
-            CGPointMake(250, 250),
-            kCGMouseButtonLeft
-    );
-    CGEventPost(kCGHIDEventTap, click1_down);
-    CGEventPost(kCGHIDEventTap, click1_up);
-    sleep(1000);
-    // Release the events
-    CFRelease(click1_up);
-    CFRelease(click1_down);
-     */
-    return true;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::K)) {
+            state = true;
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::O)) {
+            state = false;
+        }
+    }
 }
