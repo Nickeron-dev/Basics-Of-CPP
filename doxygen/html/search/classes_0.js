b3b63f6b47220119333c57ac0eb932501e5d748b
Nickeron-dev <koshkin061969@gmail.com>
2022-04-14 22:31:59 +0200

    Figured out with Doxygen.

---
+++ doxygen/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['my_5fclass_5fname_0',['my_class_name',['../classmy__class__name.html',1,'']]]
+];
---
+++ doxygen/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * \class my_class_name iostream iostream
+*/
+
+#include <iostream>
+
+class my_class_name
+{
+	int i;
+};
+
+/*!
+ * Description of a function
+ * @param number of params
+ * @param actual parameters
+*/
+int main(int argc, char **argv)
+{
+	std::cout << "Example of Doxygen\n"; /*! This line ouputs "Example of Doxygen\n" */
+	return 0;
+}
+
