3d948587dbc2fcd857465385039de49735751dc5
Nickeron-dev <koshkin061969@gmail.com>
2022-04-25 22:01:15 +0200

    Figured out one more question.

--- interview_questions/c/first.c
+++ interview_questions/c/first.c
@@ -67,5 +67,37 @@ int main(void)
 */
 
 // How to return a 2d array?
+const int n = 3;
+void print_array(int **arr)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%d \n", arr[i][j]);
+		}
+	}
+}
 
+int** generate_arr()
+{
+	int **arr = new int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = new int[n];
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = i + j;
+		}
+	}
+	return arr;
+}
+
+int main(void)
+{
+	int **arr;
+	arr = generate_arr();
+	print_array(arr);
+	return 0;
+}
 
